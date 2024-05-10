a1dad3a8202163827471c1fe2258a3d995497821
Josefpa <67612575+Josefpa@users.noreply.github.com>
2024-05-10 00:00:14 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Adjust the value as necessary
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, PATCH, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }
+        ],
+      },
+    ]
+  },
+}
